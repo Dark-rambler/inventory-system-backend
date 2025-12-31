d8bf7134e731e84425eae2ad529d758f40cbe2c9
luisoropeza <luisdavid.ingsis@gmail.com>
2025-12-30 21:03:05 -0400

    Fixing libraries

--- Inventory.Domain/Entities/Product.cs
+++ Inventory.Domain/Entities/Product.cs
@@ -5,6 +5,7 @@
         public Guid Id { get; set; }
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
+        public double Price { get; set; }
         public Category Category { get; set; } = default!;
     }
 }
