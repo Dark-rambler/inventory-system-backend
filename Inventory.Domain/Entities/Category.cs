f1512272cfbce86473a64d45237e373b0846f709
Edwin Garcia <garciaramiro000@gmail.com>
2026-01-02 19:36:52 -0400

    add description on category

--- Inventory.Domain/Entities/Category.cs
+++ Inventory.Domain/Entities/Category.cs
@@ -4,6 +4,7 @@
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
         //public ICollection<Product> Products { get; set; } = [];
     }
 }
