7a9992f75c8645fa291a8ce16c24a8efd81ac8de
luisoropeza <luisdavid.ingsis@gmail.com>
2026-01-04 17:48:47 -0400

    adding cors

--- Inventory.API/Program.cs
+++ Inventory.API/Program.cs
@@ -3,12 +3,23 @@ using Inventory.Application;
 using Inventory.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
+var CustomCors = "MyCustomCors";
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: CustomCors,
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
@@ -17,7 +28,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors(CustomCors);
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
